e8c264cefc758b01bcb53c4eff936433ec3a3396
Andreas Åström <you@example.com>
2017-04-13 21:46:16 +0200

    The app now needs a config file

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask,render_template
 app = Flask(__name__, template_folder='mac-attack/templates', static_folder='mac-attack/static')
+app.config.from_object('config')
 
 @app.route('/')
 def hello_world():
